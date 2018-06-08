305d70a6244bb0bdc02cd3d862a7d86b6a58396a
wladoytsolutions <wmoya@oytsolutions.cl>
2018-06-08 11:02:35 -0400

    .

--- www/js/index.js
+++ www/js/index.js
@@ -36,10 +36,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        alert('Received Device Ready Event');
-		FCMPlugin.getToken(function(token){
-			alert(token);
-		});
         app.setupPush();
     },
     setupPush: function() {
