9af2a9adcdc9fd742ac47011589002edff1ea95c
wladoytsolutions <wmoya@oytsolutions.cl>
2018-06-14 15:24:38 -0400

    d

--- www/js/index.js
+++ www/js/index.js
@@ -19,49 +19,31 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        this.bindEvents();
     },
-
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
     // deviceready Event Handler
     //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+        app.receivedEvent('deviceready');
     },
-
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
         console.log('Received Event: ' + id);
-        var push = PushNotification.init({
-            android: {
-            },
-            ios: {
-                alert: "true",
-                badge: true,
-                sound: 'false'
-            }
-        });
-
-        push.on('registration', function (data) {
-            alert(data.registrationId);
-            console.log(data.registrationType);
-            document.getElementById("registration").appendChild(document.createTextNode(data.registrationId));
-        });
-
-        push.on('notification', function (data) {
-            var ul = document.getElementById("pushList");
-            var li = document.createElement("li");
-            li.appendChild(document.createTextNode(data.message));
-            ul.appendChild(li);
-            console.log(data.message);
-            console.log(data.title);
-            console.log(data.count);
-            console.log(data.sound);
-            console.log(data.image);
-            console.log(data.additionalData);
-        });
     }
 };
-
-app.initialize();
