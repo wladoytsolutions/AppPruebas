dff08ed00cb0c0ad2665572f4314a1081c501213
wladoytsolutions <wmoya@oytsolutions.cl>
2019-01-09 15:39:50 -0300

    .

--- www/js/index.js
+++ www/js/index.js
@@ -17,33 +17,6 @@
  * under the License.
  */
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-		navigator.splashscreen.hide();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-    }
-};
-
-app.initialize();
-
 $(function(){
 	function camSuccess(imgData){
 		$("#img_Campurada").attr("src",imgData);
@@ -58,7 +31,7 @@ $(function(){
 	function accessCamera(){
 		var options = {
 			destinationType: Camera.DestinationType.FILE_URI,
-			sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+			sourceType: Camera.PictureSourceType.CAMERA
 		}
 		navigator.camera.getPicture(camSuccess,camError,options);
 	}
