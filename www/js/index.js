2d679e908d2df7d2964321ae9357a08c0a0ba50b
wladoytsolutions <wmoya@oytsolutions.cl>
2019-01-09 15:13:37 -0300

    -.

--- www/js/index.js
+++ www/js/index.js
@@ -38,15 +38,29 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
 
 app.initialize();
+
+$(function(){
+	function camSuccess(imgData){
+		$("#img_Campurada").attr("src",imgData);
+		
+		//<img src="file://wherehpone/img"
+	}
+	
+	function camError(error){
+		alert("Error: "+error);
+	}
+	
+	function accessCamera(){
+		var options = {
+			destinationType: Camera.DestinationType.FILE_URI,
+			sourceType: Camera.PictureSourceType.CAMERA //Photogallery
+		}
+		navigator.camera.getPicture(camSuccess,camError,options);
+	}
+  	
+	$("#btn_camara").on("click",accessCamera);
+});
