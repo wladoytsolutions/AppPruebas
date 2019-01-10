97519d45d32de9b010ca77b4fde4ccaf23ea9c1a
wladoytsolutions <wmoya@oytsolutions.cl>
2019-01-10 13:51:53 -0300

    .

--- www/js/index.js
+++ www/js/index.js
@@ -51,6 +51,20 @@ function camSuccess(imageData){
 	$('#img_Campurada').attr('src','data:image/jpeg;base64,' + imageData);
 	//$('#Base64').html(''+imageData);
 	//<img src="file://wherehpone/img"
+	SpinnerDialog.show(null, "message");
+	
+	$.post('http://35.163.42.97:8080/web/ControlJSP.jsp',{
+		Base64    : imageData,
+		Extension : 'jpeg'
+	},
+	function(response) {
+		alert(response);
+	}).done(function(response) {
+		//alert(response);
+		SpinnerDialog.hide();
+	});
+	
+	
 }
 
 function camError(error){
